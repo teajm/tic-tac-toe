043f02259c5edfd248de4e36a3bcf3b2ea3bf729
Taylor <taylormyrick@gmail.com>
2023-02-18 21:22:33 -0600

    added gameboard

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const gameBoard = (() =>{
-
-    let board = [];;
-    for(i = 0; i < 9; i++){
+    const gameboardDiv = document.querySelector('.gameboard');
+    let board = [];
+    for(let i = 0; i < 9; i++){
         board.push('');
     }
+    board.forEach((element,index) => {
+        const gameSquare = document.createElement('div');
+        gameSquare.className = 'gamesquare';
+        gameboardDiv.appendChild(gameSquare);
+    })
+    Array.from(gameboardDiv.children).forEach((gameSquare, index) => {
+        gameSquare.addEventListener('click', () =>{
+            //render player marker
+
+            //update array value to match for the active player
+
+            //remove event listener from the marked index so it can't be used
+            gameSquare.style.pointerEvents = 'none';
+            //update num of slots left on board.
+            game.slots -= 1;
+
+            //check for winner
+
+            //check remaining board slots to determine if there's a tie
+
+
+
+        })
+
+
+    });
+
 })();
+
 
 const player = (name,marker) =>{
     return {name,marker};
 }
 
 const game= (() =>{
-
+    let slots = 9;
 })();
+
